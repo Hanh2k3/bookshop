facdbf3bb21bd46361d687f8d4ffb4825e8a6979
Hanh <hanhlv.21it@vku.udn.vn>
2022-12-22 15:36:12 +0700

    fix css for heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        if(env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+
+        }
     }
 }
